e1c52dcffbbd5c817b72301b2a997ad90af9c55b
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2020-10-16 15:48:59 +0200

    classe cellule

--- src/Cellule.java
+++ src/Cellule.java
@@ -1,2 +1,48 @@
 public class Cellule {
+
+    //ATTRIBUTS
+    private int x;
+    private int y;
+
+    private int typeOccupation; //peut être libre, avec une reine, menacée par une reine
+
+    final static int LIBRE=0;
+    final static int REINE=1;
+    final static int MENACEE=2;
+
+
+    //CONSTRUCTEUR
+    public Cellule (int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    //GETTER ET SETTER
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getTypeOccupation() {
+        return typeOccupation;
+    }
+
+    public void setTypeOccupation(int typeOccupation) {
+        this.typeOccupation = typeOccupation;
+    }
+
+
 }
