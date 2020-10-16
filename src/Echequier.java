920fe2c81c27578a5156fbcb1db04a66add34d86
BrightHammer21 <lucieboucquemont@hotmail.fr>
2020-10-16 15:55:39 +0200

    Update Echequier.java

--- src/Echequier.java
+++ src/Echequier.java
@@ -1,2 +1,33 @@
 public class Echequier {
+
+    //attributs
+    private Cellule[][] echequier;
+    private int taille;
+
+    //constructeur
+    public Echequier(int taille)
+    {
+        this.taille = taille;
+        echequier = new Cellule[taille][taille];
+        initialiserEchequier();
+    }
+
+    //METHODES
+    public void initialiserEchequier()
+    {
+        for(int x=0;x<taille;x++)
+        {
+            for(int y=0;y<taille;y++)
+            {
+                echequier[x][y] = new Cellule(x,y);
+
+            }
+        }
+    }
+
+    public void modifierCellule(int x,int y, int valeur)
+    {
+        echequier[x][y].setTypeOccupation(valeur);
+    }
+
 }
